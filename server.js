5445f317d34ccd0a905a0c35c75b300d836d95f5
Sandip More <moresandip@gmail.com>
2026-03-25 22:51:39 +0530

    Fix: persist user data after refresh using localStorage

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const userModel = require('./userModel');
 
 const app = express();
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 5000;
 
 app.use(express.json());
 
